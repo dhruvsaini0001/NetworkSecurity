68a4c61d4ac4ad121a028fd24db1d9b7798721d5
dhruv <dhruvsaini2209@gmail.com>
2025-04-27 15:15:33 +0530

    create pipeline

--- networksecurity/compoents/tempCodeRunnerFile.py
+++ networksecurity/compoents/tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-networksecurity
+
+            # if tracking_url_type_store != "file":
+
+            #     # Register the model
+            #     # There are other ways to use the Model Registry, which depends on the use case,
+            #     # please refer to the doc for more information:
+            #     # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            #     mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
+            # else:
+            #     mlflow.sklearn.log_model(best_model, "model")
